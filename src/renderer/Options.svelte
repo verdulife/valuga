ec0a65f0fd037616ad27d626daad8382b4d9d555
verdulife <verdu@live.com>
2022-04-06 22:25:37 +0200

    add plugins

--- src/renderer/Options.svelte
+++ src/renderer/Options.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import type { themeType } from "@lib/types";
-  import { themeStore } from "@lib/store";
+  import type { themeType, pluginType } from "@lib/types";
+  import { themeStore, pluginStore } from "@lib/store";
 
   function installTheme() {
     const input = document.createElement("input");
@@ -41,30 +41,92 @@
       return themeItem;
     });
   }
+
+  function installPlugin() {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".json";
+
+    input.onchange = () => {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const data = JSON.parse(reader.result);
+        data.active = false;
+        const pluginExists = $pluginStore.some((plugin: pluginType) => plugin.name === data.name);
+
+        if (pluginExists) {
+          alert("This theme already exists");
+          return;
+        }
+
+        $pluginStore = [...$pluginStore, data];
+      };
+
+      reader.readAsText(file);
+    };
+
+    input.click();
+  }
+
+  function togglePlugin(plugin: pluginType) {
+    $pluginStore = $pluginStore.map((pluginItem: pluginType) => {
+      if (pluginItem.name === plugin.name) {
+        pluginItem.active = !pluginItem.active;
+      }
+
+      return pluginItem;
+    });
+  }
 </script>
 
-<div class="options col fill">
-  <header class="row jbetween acenter xfill">
-    <h2>Themes</h2>
-    <button class="install-btn" on:click={installTheme}>NEW THEME</button>
-  </header>
+<div class="scroll">
+  <div class="options col fill">
+    <header class="row jbetween acenter xfill">
+      <h2>Themes</h2>
+      <button class="install-btn" on:click={installTheme}>NEW THEME</button>
+    </header>
 
-  <ul class="col xfill">
-    {#each $themeStore as theme}
-      <li class="row jbetween acenter xfill">
-        <div class="col">
-          <h3>{theme.name}</h3>
-          <small>{theme.description}</small>
-        </div>
+    <ul class="col xfill">
+      {#each $themeStore as theme}
+        <li class="row jbetween acenter xfill">
+          <div class="col">
+            <h3>{theme.name}</h3>
+            <small>{theme.description}</small>
+          </div>
 
-        {#if theme.active}
-          <p class="label">ACTIVE</p>
-        {:else}
-          <button class="activate-btn" on:click={() => changeTheme(theme)}>USE THEME</button>
-        {/if}
-      </li>
-    {/each}
-  </ul>
+          {#if theme.active}
+            <p class="label">ACTIVE</p>
+          {:else}
+            <button class="activate-btn" on:click={() => changeTheme(theme)}>USE THEME</button>
+          {/if}
+        </li>
+      {/each}
+    </ul>
+
+    <header class="row jbetween acenter xfill">
+      <h2>Plugins</h2>
+      <button class="install-btn" on:click={installPlugin}>NEW PLUGIN</button>
+    </header>
+
+    <ul class="col xfill">
+      {#each $pluginStore as plugin}
+        <li class="row jbetween acenter xfill">
+          <div class="col">
+            <h3>{plugin.name}</h3>
+            <small>{plugin.description}</small>
+          </div>
+
+          {#if plugin.active}
+            <p class="label">ACTIVE</p>
+          {:else}
+            <button class="activate-btn" on:click={() => togglePlugin(plugin)}>ACTIVATE</button>
+          {/if}
+        </li>
+      {/each}
+    </ul>
+  </div>
 </div>
 
 <style lang="scss">
@@ -75,7 +137,7 @@
   }
 
   header {
-    margin-bottom: 40px;
+    margin-bottom: 20px;
   }
 
   button {
@@ -86,6 +148,10 @@
 
   .install-btn {
     background: $link;
+  }
+
+  ul {
+    margin-bottom: 80px;
   }
 
   li {
